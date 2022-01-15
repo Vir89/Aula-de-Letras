95279fd11f7d169bbfc27eac9085e9c2d468cb5e
Vir89 <virisabelherreraherrera@hotmail.com>
2022-01-15 18:41:42 +0100

    new changes added

--- adl/src/App.js
+++ adl/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navbar />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
